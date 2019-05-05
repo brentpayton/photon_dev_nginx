3e0f2965cee3c750ea50b2811e6ed079607e3c2e
Brent Payton <brent@brentpayton.com>
2019-05-05 13:38:59 +0000

    Adding files from Node site.

---
+++ gruntfile.js
@@ -0,0 +1,102 @@
+module.exports = function(grunt) {
+
+// Project Configuration
+grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
+	uglify: {
+		options: {
+			banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+		},
+		build: {
+        	src: 'src/<%= pkg.name %>.js',
+   	    	dest: 'build/<%= pkg.name %>.min.js'
+		}
+	},
+	run: {
+		wget: {
+			cmd: 'wget',
+			args: [
+				'--config=wgetrc',
+				'photon-art.com'
+			]
+		},
+		blc: {
+			cmd: './node_modules/.bin/blc',
+			args: [
+				'https://photon-art.com',
+				'--filter-level 3 ',
+				'--ordered ',
+				'--recursive ',
+				'--exclude *.php',
+				'--verbose',
+				'--follow'
+			]
+		}
+	},
+	htmllint: {
+		root: {
+			src: "temp/*.html"
+		},
+		all: {
+			options: {
+				force: true
+			},
+			src: [ 'temp/**/*.html' ]
+		},
+	},
+	bootlint: {
+		options: {
+			showallerrors: true
+		},
+		files: ['temp/**/*.html']
+	},
+	csslint: {
+		src: ['temp/css/*.css']
+	},
+	linkChecker: {
+		options: {
+			maxConcurrency: 20
+		},
+	  default: {
+	    site: 'photon-art.com',
+	    options: {
+	      initialPort: 443
+	    }
+	  }
+	}
+});
+
+	grunt.registerTask('test', function () {
+	    var tasks = [
+		    'run:wget',
+				'htmllint:all',
+				'bootlint',
+				'csslint'
+	    ];
+
+	    tasks.forEach(function (task) {
+	        grunt.task.run(task);
+	    });
+	});
+
+	// Load the plugin that profides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	// Default tasks.
+	grunt.registerTask('default', ['uglify']);
+
+	// grunt-html for linting html.
+	grunt.loadNpmTasks('grunt-html');
+
+	// grunt-run for calling executables, shell scripts, etc.  Needed in order to run wget in particular.
+	grunt.loadNpmTasks('grunt-run');
+
+	// Linter for twitter bootstrap.
+	grunt.loadNpmTasks('grunt-bootlint');
+
+	// Linter for css files
+	grunt.loadNpmTasks('grunt-contrib-csslint');
+
+	// Grunt dead link checker
+	grunt.loadNpmTasks('grunt-link-checker');
+};
